fd3eb24b837eaae473cc65c3dd903fbb98aaf352
SaraNoori <sn1044604@student.francistuttle.edu>
2024-03-30 21:41:02 -0500

    finished project
    finished the project. Maybe I will clean up later

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,5 +1,59 @@
 "use strict";
 
+var rockASCII =
+    "    _______<br />---'   ____)<br />      (_____)<br />      (_____)<br />      (____)<br />---.__(___)";
+
+var paperASCII =
+    "    _______<br />---'   ____)____<br />          ______)<br />          _______)<br />          _______)<br />---.__________)<br />"
+
+var scissorsASCII =
+    "    _______<br />---'   ____)____<br />          ______)<br />        __________)<br />      (____)<br />---.__(___)"
+
+
+
+
+
+var computerWins = 0;
+var playerWins = 0;
+
+const rockBut = document.querySelector(".rockBut");
+const scissorsBut = document.querySelector(".scissorsBut");
+const paperBut = document.querySelector(".paperBut");
+const historyLog = document.querySelector(".moveHist");
+const plWinCount = document.querySelector(".playerWin");
+const compWinCount = document.querySelector(".computerWin");
+const playerArt = document.querySelector(".playerArt");
+const computerArt = document.querySelector(".computerArt");
+const restartBut = document.querySelector(".restartButton");
+
+rockBut.addEventListener("click", processSelection);
+scissorsBut.addEventListener("click", processSelection);
+paperBut.addEventListener("click", processSelection);
+restartBut.addEventListener("click", restart);
+
+playerArt.innerHTML = rockASCII;
+
+function restart()
+{
+    playerWins = 0;
+    computerWins = 0;
+    plWinCount.textContent = "Player: " + playerWins;
+    compWinCount.textContent = "Computer: " + computerWins;
+
+    playerArt.innerHTML = rockASCII;
+    historyLog.replaceChildren();
+}
+
+
+
+function processSelection(event)
+{
+    var buttonType = event.target.getAttribute("class");
+    var compChoice = getComputerChoice();
+    var result = playRound(buttonType, compChoice);
+    processReult(result); 
+}
+
 function getComputerChoice()
 {
     let options = ["rock", "paper", "scissors"];
@@ -10,7 +64,11 @@ function getComputerChoice()
 function playRound(playerSelection, computerSelection)
 {
     playerSelection.toLowerCase();
-    console.log("The computer chose: " + computerSelection);
+    playerSelection = playerSelection.split("But")[0];
+    changePlayerArt(playerSelection);
+
+    addToHist("The computer Chose: " + computerSelection);
+    
     if (playerSelection === computerSelection)
     {
         return "draw";
@@ -35,39 +93,77 @@ function playRound(playerSelection, computerSelection)
     }
 
     return "I don't recognize that move";
+
+
 }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
-
-function playGame(rounds)
+function processReult(result)
 {
-    let computerWins = 0;
-    let playerWins = 0;
-
-    for (let i = 1; i <= rounds; i++)
+    if (result == "win")
     {
-        let playerSelection = prompt("game " + i + ": Chose Rock, Paper, or Scissors", "");
-        let computerSelection = getComputerChoice();
-        let theRound = playRound(playerSelection, computerSelection);
+        playerWins += 1;
+        addSpecialHist("You Win!");
+    }
+    else if (result == "lose")
+    {
+        computerWins += 1;
+        addSpecialHist("You lose!");
+    }
+    else
+    {
+        addSpecialHist("It's a draw!");
+    }
 
-        if ( theRound === "win") 
-        {
-            playerWins += 1; 
-            console.log("you win!");
-        }
-        else if (theRound === "lose")
-        {
-            computerWins += 1;
-            console.log("you lose!");
-        }
-        else console.log("it's a draw!");
+    plWinCount.textContent = "Player: " + playerWins;
+    compWinCount.textContent = "Computer: " + computerWins;
 
-        console.log("Player: " + playerWins);
-        console.log("Computer: " + computerWins);
-        console.log("\n");
+}
+
+
+function addToHist(text)
+{
+    removeOldHist();
+    const log = document.createElement("li");
+    log.textContent = text;
+    historyLog.appendChild(log);
+}
+
+function addSpecialHist(text)
+{
+    removeOldHist();
+    const log = document.createElement("li");
+    log.style.color = "orange";
+    log.textContent = "---- " + text + " ----";
+    historyLog.appendChild(log);
+}
+
+
+function removeOldHist()
+{
+    if (historyLog.children.length > 5)
+    {
+        historyLog.firstElementChild.remove();
+        historyLog.firstElementChild.remove();
     }
 }
 
-//playGame(5);
+function changePlayerArt(move)
+{
+    if (move == "rock")
+    {
+        playerArt.innerHTML = rockASCII;
+        return;
+    }
+
+    if (move == "paper")
+    {
+        playerArt.innerHTML = paperASCII;
+        return;
+    }
+
+    if (move == "scissors")
+    {
+        playerArt.innerHTML = scissorsASCII;
+        return;
+    }
+}
